18363b16aa842f4f8dcc8e30f07e43ed959b26f5
jaemanc <jaemanc93@gmail.com>
2023-06-28 11:04:35 +0900

    정렬
    1. h-index 추가.

--- src/algo/이분검색및정렬/H_index.java
+++ src/algo/이분검색및정렬/H_index.java
@@ -1,4 +1,56 @@
 package algo.이분검색및정렬;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class H_index {
+    /*
+    H-Index는 과학자의 생산성과 영향력을 나타내는 지표입니다. 어느 과학자의 H-Index를 나타내는 값인 h를 구하려고 합니다. 위키백과1에 따르면, H-Index는 다음과 같이 구합니다.
+    어떤 과학자가 발표한 논문 n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-Index입니다.
+    어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 이 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요.
+
+    제한사항
+    과학자가 발표한 논문의 수는 1편 이상 1,000편 이하입니다.
+    논문별 인용 횟수는 0회 이상 10,000회 이하입니다.
+     */
+
+    public static void main(String[] args) {
+        // citations	            return
+        // [3, 0, 6, 1, 5]	      3
+        // int[] citations = {6, 6, 1, 4, 4, 1, 1, 1, 1, 5, 6};
+        // int[] citations = {25, 8, 5, 3, 3}; -> 3 리턴
+        int[] citations = {100, 100, 100}; // -> 3 리턴해야 함 ( 테케 9번 )
+
+        System.out.println(solution(citations));
+    }
+
+    public static int solution(int[] citations) {
+        int answer = 0;
+
+        Integer[] temp = Arrays.stream(citations).boxed().toArray(Integer[]::new);
+        Arrays.sort(temp,Collections.reverseOrder());
+        for (int i=0; i<temp.length; i++) {
+            if (i+1 > temp[i]) {
+                answer = i;
+                return answer;
+            }
+        }
+
+        if (answer == 0) {
+            return citations.length;
+        }
+
+        return answer;
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
